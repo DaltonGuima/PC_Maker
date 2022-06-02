cd6f9b29ea580ec225d5a8b601c375e2d88c9e8b
Your Name <you@example.com>
2022-06-02 18:44:52 -0300

    Vamo que vamo

--- js/script.js
+++ js/script.js
@@ -13,5 +13,38 @@ function pesquisar(){
 
 }
 
+function carousel(){
+  const carousel = document.getElementById("carousel"),
+    content = document.getElementById("content"),
+    next = document.getElementById("next"),
+    prev = document.getElementById("prev");
+  
+  next.addEventListener("click", e => {
+    carousel.scrollBy(width, 0);
+    if (carousel.scrollWidth !== 0) {
+      prev.style.display = "flex";
+    }
+    if (content.scrollWidth - width <= carousel.scrollLeft + width) {
+      next.style.display = "none";
+    }
+  });
+  
+  prev.addEventListener("click", e => {
+    carousel.scrollBy(-width, 0);
+    if (carousel.scrollLeft - width <= 0) {
+      prev.style.display = "none";
+    }
+    if (!content.scrollWidth - width <= carousel.scrollLeft + width) {
+      next.style.display = "flex";
+    }
+  });
+  
+  let width = carousel.offsetWidth;
+  window.addEventListener("resize", e => (width = carousel.offsetWidth));
+}
 
-
+function collapse(){
+  $('.btn-expand-collapse').click(function(e) {
+    $('.navbar-primary').toggleClass('col-md-1');
+});
+}
