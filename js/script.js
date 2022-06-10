1fef79b3546de89ef7649e803f68c7b0bf2435d0
Your Name <you@example.com>
2022-06-10 15:24:20 -0300

    Modals

--- js/script.js
+++ js/script.js
@@ -116,3 +116,22 @@ function CancelModify(){
 
 }
 
+/* Modal */
+
+
+  function Modaltext(){
+
+    var Modal = document.getElementById('ModalFoto')
+    Modal.addEventListener('show.bs.modal', function (event) {
+
+    var button = event.relatedTarget
+
+    var name = button.getAttribute('data-bs-name')
+    var foto = button.getAttribute('data-bs-foto')
+
+    var modalTitle = Modal.querySelector('.modal-title')
+    var modalBodyImg = Modal.querySelector('.modal-body img')
+
+    modalTitle.textContent = name
+    modalBodyImg.src = foto
+})}
