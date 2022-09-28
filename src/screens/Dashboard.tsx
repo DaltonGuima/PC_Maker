2f910d2682fa85b2065ebd8f79999166ef835e58
DaltonGuima <dalton.guimaraes500@gmail.com>
2022-09-28 11:17:52 -0300

    Ultimo commit da aula

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -56,18 +56,6 @@ function Dashboard() {
                                     </a>
                                 </li>
                                 <li className="has-sub">
-                                    <a className="js-arrow" data-bs-toggle="collapse" data-bs-target="#vendedor" role="button" aria-expanded="false" aria-controls="componentes">
-                                        <i className="fa-solid fa-shop"></i>Vendedor</a>
-                                    <ul className="list-unstyled navbar__sub-list js-sub-list collapse" id="vendedor">
-                                        <li>
-                                            <a href="/componentes"><i className="fas fa-table"></i>Tabelas</a>
-                                        </li>
-                                        <li>
-                                            <a href="/insercao"><i className="fa-solid fa-circle-plus"></i>Inserção</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li className="has-sub">
                                     <a className="js-arrow" data-bs-toggle="collapse" data-bs-target="#componentes" role="button" aria-expanded="false" aria-controls="componentes">
                                         <i className="fa-solid fa-puzzle-piece"></i>Componentes</a>
                                     <ul className="list-unstyled navbar__sub-list js-sub-list collapse" id="componentes">
