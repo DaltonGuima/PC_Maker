645ea4edc50750e43a849159c1ac95d68a13aa3b
DaltonGuima <dalton.guimaraes500@gmail.com>
2023-05-23 09:09:57 -0300

    Arrumei algumas coisas

--- PC_Maker/app/components/TableProduct.tsx
+++ PC_Maker/app/components/TableProduct.tsx
@@ -1,22 +1,36 @@
-import type { Componente, GabineteProps, VentoinhaProps } from "~/Interface/ComponenteInterface";
+import type { Componente, GabineteProps, VentoinhaProps, TecladoProps } from "~/Interface/ComponenteInterface";
 
 export default function TableProduct(props: { produto: Componente | undefined }) {
+
+    const map = new Map()
     switch (props.produto?.categoria) {
         case 'Ventoinha':
-            return (
-                <>
-                    <th className="specsBarraLatTh">Tamanho</th>
-                    <td className="specsBarraLatTd">{(props.produto as VentoinhaProps).especificacoes.tamanho}</td>
-                </>
-            )
+            map.set("Tamanho", (props.produto as VentoinhaProps).especificacoes.tamanho)
+        case 'Teclado':
+            map.set("Tamanho", (props.produto as TecladoProps).especificacoes.tamanho)
+            map.set("Tipo", (props.produto as TecladoProps).especificacoes.tipo)
+        case "Armazenamento":
+            map.set("Tipo", (props.produto as TecladoProps).especificacoes.tipo)
+            map.set("Tamanho", (props.produto as TecladoProps).especificacoes.tamanho)
+            map.set("Tamanho", (props.produto as TecladoProps).especificacoes.tamanho)
+            map.set("Tamanho", (props.produto as TecladoProps).especificacoes.tamanho)
         case 'Gabinete':
-            return (
-                <>
-                    <th className="specsBarraLatTh">Tamanho</th>
-                    <td className="specsBarraLatTd">{(props.produto as GabineteProps).especificacoes.tipo}</td>
-                </>
-            )
-        default:
-            return null
+            map.set("Tipo", (props.produto as GabineteProps).especificacoes.tipo)
     }
+
+
+    return (
+        <>
+            {map.forEach(function (value, key) {
+                return (
+                    <>
+                        <th className="specsBarraLatTh">{key}</th>
+                        <td className="specsBarraLatTd">{value}</td>
+                    </>
+                )
+            })}
+        </>
+    )
+
+
 }
